a7bc83915600295024ff1b176ac8e175640dc9db
Dhionathan lanzoni Jobim <dhionathan01@hotmail.com>
2025-10-08 09:54:02 -0300

    ✨ feat: adiciona componente de cabeçalho
    Cria o componente Heading com estilos personalizados.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import './styles/theme.css';
 import './styles/global.css';
+import { Heading } from './components/Heading';
 export function App() {
   return (
     <>
+      <Heading />
       <h1>Hello World</h1>
       <h1>Olá Mundo</h1>
       <p>Dhionathan Jobim</p>

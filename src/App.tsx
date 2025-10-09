d8ae2be865ed6401324b61f116c387b7bdcb2ae5
Dhionathan lanzoni Jobim <dhionathan01@hotmail.com>
2025-10-09 09:33:07 -0300

    🐛 fix: remove conteúdo do componente App
    Remove o conteúdo do componente App, deixando-o vazio.

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,6 @@
 import './styles/theme.css';
 import './styles/global.css';
-import { Heading } from './components/Heading';
-import { TimerIcon } from 'lucide-react';
+
 export function App() {
-  return (
-    <>
-      <Heading>
-        Dhionathan Jobim
-        <button>
-          <TimerIcon />
-        </button>
-      </Heading>
-      <h1>Hello World</h1>
-      <h1>Olá Mundo</h1>
-      <p>Dhionathan Jobim</p>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo, veniam
-        mollitia eaque beatae accusamus itaque? Doloremque provident dolores
-        commodi facere incidunt consectetur enim nam, consequatur accusantium?
-        Vel adipisci sapiente quidem!
-      </p>
-    </>
-  );
+  return <></>;
 }

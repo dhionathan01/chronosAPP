81dbd96e60aef38c06322d02c99368defa756a83
Dhionathan lanzoni Jobim <dhionathan01@hotmail.com>
2025-10-09 00:52:18 -0300

    ✨ feat: adiciona tipagem e estilos ao componente Heading
    Adiciona a tipagem de propriedades e aplica estilos ao componente Heading.

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,5 +1,9 @@
 import styles from './Heading.module.css';
 
-export function Heading(props) {
-  return <h1>{props.children}</h1>;
+type HeadingProps = {
+  children: string;
+};
+
+export function Heading({ children }: HeadingProps) {
+  return <h1 className={styles.heading}>{children}</h1>;
 }

247917acff939167735f772ded77a3ac4f667818
Dhionathan lanzoni Jobim <dhionathan01@hotmail.com>
2025-10-09 00:22:19 -0300

    ✨ feat: adiciona propriedades ao componente Heading
    Agora o componente Heading aceita propriedades para exibir conteúdo dinâmico.

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,18 @@ import { Heading } from './components/Heading';
 export function App() {
   return (
     <>
-      <Heading />
+      <Heading attr={123} attr2={'String teste'}>
+        Dhionathan Jobim
+      </Heading>
       <h1>Hello World</h1>
       <h1>Olá Mundo</h1>
       <p>Dhionathan Jobim</p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo, veniam
+        mollitia eaque beatae accusamus itaque? Doloremque provident dolores
+        commodi facere incidunt consectetur enim nam, consequatur accusantium?
+        Vel adipisci sapiente quidem!
+      </p>
     </>
   );
 }
--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,7 +1,7 @@
 import styles from './Heading.module.css';
 
-export function Heading() {
+export function Heading(props) {
   return (
-    <h1 className={`${styles.heading} ${styles.beige}`}>{'Heading Element'}</h1>
+    <h1 className={`${styles.heading} ${styles.beige}`}>{props.children}</h1>
   );
 }

c4716d15e8d356f09a9ab5c3c54c02b2304397ae
Dhionathan lanzoni Jobim <dhionathan01@hotmail.com>
2025-10-09 00:42:31 -0300

    🚫 chore: remove estilos e variáveis não utilizados
    Removido código CSS desnecessário dos componentes Heading e estilos globais.

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,10 +1,5 @@
 import styles from './Heading.module.css';
 
 export function Heading(props) {
-  console.log(props);
-  return (
-    <h1 className={`${styles.heading} ${styles.beige}`} style={props.style}>
-      {props.children}
-    </h1>
-  );
+  return <h1>{props.children}</h1>;
 }

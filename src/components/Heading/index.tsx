b0364a266ce7563f00dc35af52241b873067ed4a
Dhionathan lanzoni Jobim <dhionathan01@hotmail.com>
2025-10-09 23:34:19 -0300

    refactor: :recycle: Refatorando chamada dos components
    Deixando todo o component dentro do seu index e removendo o arquivo com nome equivalente

--- src/components/Heading/index.tsx
+++ src/components/Heading/index.tsx
@@ -1,1 +1,9 @@
-export { Heading } from './Heading';
+import styles from './Heading.module.css';
+
+type HeadingProps = {
+  children: React.ReactNode;
+};
+
+export function Heading({ children }: HeadingProps) {
+  return <h1 className={styles.heading}>{children}</h1>;
+}

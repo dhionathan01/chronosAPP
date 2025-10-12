5375a33b666d595df93c7d29d2f3963e94281729
Dhionathan lanzoni Jobim <dhionathan01@hotmail.com>
2025-10-12 00:48:06 -0300

    refactor: :recycle: Refatorando component menu para seguir o padrão do curso

--- Preview.md
+++ Preview.md
@@ -1,3 +1,5 @@
 - Implementando a Logo ![alt text](preview/image.png)
 
 - Minha tentativa de fazer o menu: ![alt text](preview/image2.png)
+
+- Professor / Seguindo curso ![alt text](preview/image3.png)
--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,20 +1,20 @@
-import { HistoryIcon, HomeIcon, SettingsIcon, SunIcon } from 'lucide-react';
+import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from 'lucide-react';
 import styles from './styles.module.css';
 export function Menu() {
   return (
-    <div className={styles.menuDiv}>
-      <a href='#' className={`${styles.iconRound} ${styles.activate}`}>
-        <HomeIcon />
+    <nav className={styles.menu}>
+      <a href='#' className={styles.menuLink}>
+        <HouseIcon />
       </a>
-      <a href='#' className={styles.iconRound}>
+      <a href='#' className={styles.menuLink}>
         <HistoryIcon />
       </a>
-      <a href='#' className={styles.iconRound}>
+      <a href='#' className={styles.menuLink}>
         <SettingsIcon />
       </a>
-      <a href='#' className={styles.iconRound}>
+      <a href='#' className={styles.menuLink}>
         <SunIcon />
       </a>
-    </div>
+    </nav>
   );
 }

023d3534083df59dc657816316271a4c0a7dcf65
Dhionathan lanzoni Jobim <dhionathan01@hotmail.com>
2025-11-19 15:19:02 -0300

    ✨ feat: adiciona páginas AboutPomodoro e NotFound com layout genérico Adiciona os componentes AboutPomodoro e NotFound, utilizando o componente GenericHtml para estruturação do conteúdo e estilização.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { Home } from './pages/home';
 import './styles/theme.css';
 import './styles/global.css';
+import { NotFound } from './pages/NotFound';
+import { AboutPomodoro } from './pages/AboutPomodoro';
 
 export function App() {
   return <Home />;

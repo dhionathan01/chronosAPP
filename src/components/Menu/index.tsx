8c0ded4de20f95493527781a2106d7932993d4c1
Dhionathan lanzoni Jobim <dhionathan01@hotmail.com>
2025-11-19 12:58:25 -0300

    ✨ feat: adiciona suporte a temas claro e escuro
    Implementa a troca de temas na aplicação, permitindo que o usuário alterne entre os modos claro e escuro. Adiciona variáveis de tema no CSS e atualiza o componente Menu para gerenciar a mudança de tema.

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,18 +1,54 @@
 import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from 'lucide-react';
 import styles from './styles.module.css';
+import { useEffect, useState } from 'react';
+
+type AvaibledThemes = 'dark' | 'light';
+
 export function Menu() {
+  const [theme, setTheme] = useState<AvaibledThemes>('dark');
+  function handleChangeTheme(
+    event: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
+  ) {
+    event.preventDefault();
+    setTheme(prevState => (prevState === 'dark' ? 'light' : 'dark'));
+  }
+
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', theme);
+  }, [theme]);
   return (
     <nav className={styles.menu}>
-      <a href='#' className={styles.menuLink}>
+      <a
+        href='#'
+        className={styles.menuLink}
+        aria-label='Ir para home'
+        title='Navegar para a home.'
+      >
         <HouseIcon />
       </a>
-      <a href='#' className={styles.menuLink}>
+      <a
+        href='#'
+        className={styles.menuLink}
+        aria-label='Ir para histórico'
+        title='Navegar para o histórico'
+      >
         <HistoryIcon />
       </a>
-      <a href='#' className={styles.menuLink}>
+      <a
+        href='#'
+        className={styles.menuLink}
+        aria-label='Ir para as configurações'
+        title='Ir para as configurações'
+      >
         <SettingsIcon />
       </a>
-      <a href='#' className={styles.menuLink}>
+      <a
+        href='#'
+        className={styles.menuLink}
+        aria-label='Alterar o tema'
+        title='Alterar o tema'
+        onClick={event => handleChangeTheme(event)}
+      >
         <SunIcon />
       </a>
     </nav>

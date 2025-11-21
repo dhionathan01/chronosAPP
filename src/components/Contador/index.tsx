598df9fa0cc97511c26191df5f49fa7c049a4747
Dhionathan lanzoni Jobim <dhionathan01@hotmail.com>
2025-11-21 16:48:24 -0300

    🐛 fix: remove funcionalidade de alteração de tempo no contador
    Remove a função de alteração de tempo e o botão correspondente do componente Contador.

--- src/components/Contador/index.tsx
+++ src/components/Contador/index.tsx
@@ -1,19 +1,9 @@
 import { useTaskContext } from '../../contexts/TaskContext/useTaskContext';
 import styles from './styles.module.css';
 export function Contador() {
-  const { state, setState } = useTaskContext();
-  function handleClickAlterTime() {
-    setState(prevState => {
-      return {
-        ...prevState,
-        formattedSecondsRemaining: '21:00',
-      };
-    });
-  }
-
+  const { state } = useTaskContext();
   return (
     <div>
-      <button onClick={handleClickAlterTime}>click</button>
       <div className={styles.container}>{state.formattedSecondsRemaining}</div>
     </div>
   );

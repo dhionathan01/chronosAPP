7aa7c28b9cab7e872776cefdb30d9ffc29300f0c
Dhionathan lanzoni Jobim <dhionathan01@hotmail.com>
2025-11-21 17:26:49 -0300

    feat: :sparkles: Criando um input controlado e um botão de submit para logar o conteúdo do valor preenchido no input

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -2,10 +2,17 @@ import { PlayCircleIcon } from 'lucide-react';
 import { DefaultInput } from '../DefaultInput';
 import { DefaultButton } from '../DefaultButton';
 import { Cycles } from '../Cycles';
+import { useState } from 'react';
 
 export function MainForm() {
+  const [taskName, setTaskName] = useState('');
+  function handleClickSubmitForm(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    console.log(event);
+    console.log(taskName);
+  }
   return (
-    <form className='form' action=''>
+    <form className='form' action='' onSubmit={handleClickSubmitForm}>
       <div className='formRow'>
         <DefaultInput
           id={'taskInput'}
@@ -13,6 +20,8 @@ export function MainForm() {
           labelText='Task'
           placeholder='Digite o nome da task...'
           title='Input Text Title'
+          onChange={e => setTaskName(e.target.value)}
+          value={taskName}
         />
       </div>
       <div className='formRow'>

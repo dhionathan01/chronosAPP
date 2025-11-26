2a7dac95e2dbcfa63e6374cb3eceaea1e450b018
Dhionathan lanzoni Jobim <dhionathan01@hotmail.com>
2025-11-25 23:18:40 -0300

    ✨ feat: adiciona funcionalidade de criação de tarefas no formulário principal
    Implementa a lógica para adicionar novas tarefas ao estado global, incluindo a definição de propriedades como id, nome, datas e tipo de tarefa. Também corrige a verificação de entrada no campo de nome da tarefa.

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -3,14 +3,39 @@ import { DefaultInput } from '../DefaultInput';
 import { DefaultButton } from '../DefaultButton';
 import { Cycles } from '../Cycles';
 import { useRef } from 'react';
+import type { TaskModel } from '../../models/TaskModel';
+import { useTaskContext } from '../../contexts/TaskContext/useTaskContext';
 
 export function MainForm() {
-  const taskNameInput = useRef(null);
+  const { state, setState } = useTaskContext();
+  const taskNameInput = useRef<HTMLInputElement>(null);
   function handleClickSubmitForm(event: React.FormEvent<HTMLFormElement>) {
-    event.preventDefault();
-    console.log(event);
     console.log(taskNameInput.current);
     console.log(taskNameInput.current.value);
+    event.preventDefault();
+    if (!taskNameInput.current) return;
+    const taskName = taskNameInput.current.value.trim();
+    if (!taskName) {
+      console.log('entri no !taskname');
+      return;
+    }
+    const newTask: TaskModel = {
+      id: Date.now().toString(),
+      name: taskName,
+      startDate: Date.now(),
+      completeDate: null,
+      interruptDate: null,
+      duration: 1,
+      type: 'worktime',
+    };
+    setState(prevState => {
+      return {
+        ...prevState,
+        tasks: [...prevState.tasks, newTask],
+        activeTask: newTask,
+        currentCycle: 1,
+      };
+    });
   }
   return (
     <form className='form' action='' onSubmit={handleClickSubmitForm}>

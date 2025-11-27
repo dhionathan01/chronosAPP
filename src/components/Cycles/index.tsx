c0d09bdf77becb65ef74c06a34d94a5f53fbed40
Dhionathan lanzoni Jobim <dhionathan01@hotmail.com>
2025-11-27 13:27:06 -0300

    feat: :sparkles: Aplicando funcionalidade de listagem e iteração de ciclos

--- src/components/Cycles/index.tsx
+++ src/components/Cycles/index.tsx
@@ -1,9 +1,16 @@
 import { useEffect } from 'react';
 import { useTaskContext } from '../../contexts/TaskContext/useTaskContext';
 import styles from './styles.module.css';
+import { getNextCycle } from '../../utils/getNextCycle';
+import { getNextCycleType } from '../../utils/getNextCycleType';
 export function Cycles() {
-  const { state, setState } = useTaskContext();
+  const { state } = useTaskContext();
   const cycleStep = Array.from({ length: state.currentCycle });
+  const cycleNames = {
+    workTime: 'foco',
+    shortBreakTime: 'descanso',
+    longBreakTime: 'descanso Longo',
+  };
   console.log(cycleStep);
   useEffect(() => {
     console.log(state);
@@ -12,16 +19,18 @@ export function Cycles() {
     <div className={styles.cycles}>
       <span>Ciclos:</span>
       <div className={styles.cycleDots}>
-        {state.tasks &&
-          state.tasks.map(item => {
-            console.log(item);
-            console.log(item['type']);
-            return (
-              <span
-                className={`${styles.cycleDot} ${styles[item['type']]}`}
-              ></span>
-            );
-          })}
+        {cycleStep.map((_, index) => {
+          const nextCycle = getNextCycle(index);
+          const nextCycleType = getNextCycleType(nextCycle);
+          return (
+            <span
+              key={`${nextCycle}`}
+              className={`${styles.cycleDot} ${styles[nextCycleType]}`}
+              aria-label={`Indicador de ciclo de ${cycleNames[nextCycleType]}`}
+              title={`Indicador de ciclo de ${cycleNames[nextCycleType]}`}
+            ></span>
+          );
+        })}
       </div>
     </div>
   );

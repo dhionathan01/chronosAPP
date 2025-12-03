12970b83bce6848277b83c6f9a444070de49a0b1
Dhionathan lanzoni Jobim <dhionathan01@hotmail.com>
2025-12-03 16:31:38 -0300

    ✨ feat: adiciona gerenciamento de worker de tempo em padrão singleton Implementa a classe TimeWorkerManager para gerenciar um Web Worker, permitindo a comunicação e a terminação do worker.

---
+++ src/workers/TimeWorkerManager.ts
@@ -0,0 +1,28 @@
+let instance: TimeWorkerManager | null = null;
+export class TimeWorkerManager {
+  private worker: Worker;
+
+  private constructor() {
+    this.worker = new Worker(new URL('./timeWorker.js', import.meta.url));
+  }
+
+  public static getInstance() {
+    if (!instance) {
+      instance = new TimeWorkerManager();
+    }
+    return instance;
+  }
+
+  postMessage(message: any) {
+    this.worker.postMessage(message);
+  }
+
+  onmessage(cb: (e: MessageEvent) => void) {
+    this.worker.onmessage = cb;
+  }
+
+  terminate() {
+    this.worker.terminate();
+    instance = null;
+  }
+}
---
+++ src/workers/timeWorker.js
@@ -0,0 +1,4 @@
+self.onmessage = function (event) {
+  console.log(event);
+  console.log(event.data);
+};
